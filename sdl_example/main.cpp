bbec37b1e7ccf68d188fef1b623ba3420a31c1fb
Igor Gridchyn <igor.gridchin@gmail.com>
2014-02-10 17:35:37 +0100

    Initial Commit

---
+++ sdl_example/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  sdl_example
+//
+//  Created by Igor Gridchyn on 10/02/14.
+//  Copyright (c) 2014 Igor Gridchyn. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
