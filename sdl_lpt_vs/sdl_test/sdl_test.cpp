5aabd4974533f730449537ff672d44014665fad4
unknown <data@C0410048.ist.local>
2014-02-25 10:54:06 +0100

    Move inpout32 functionality

--- sdl_lpt_vs/sdl_test/sdl_test.cpp
+++ sdl_lpt_vs/sdl_test/sdl_test.cpp
@@ -1,250 +1,17 @@
 // sdl_test.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+#include "inpout32.h"
 #include "SDL.h"
-#include <windows.h>
-#include <WinBase.h>
-#include <winioctl.h>
-#include <conio.h>
-#include <winsvc.h>
-#include <minwindef.h>
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
-
-#define IDR_BIN1                        101
 
 struct WRITE_IO_PORT_INPUT
 {
 	ULONG PortNumber;
 	UCHAR CharData;
 };
-
-char path[MAX_PATH];
-HINSTANCE hmodule;
-
-// was LPCTSTR
-// isntall a driver for the LPT port
-int inst(LPCSTR pszDriver)
-{
-	char szDriverSys[MAX_PATH];
-	strcpy_s(szDriverSys, MAX_PATH, pszDriver);
-	strcat_s(szDriverSys, MAX_PATH, ".sys\0");
-
-	SC_HANDLE  Mgr;
-	SC_HANDLE  Ser;
-	GetSystemDirectoryA(path, sizeof(path));
-	HRSRC hResource = FindResource(hmodule, MAKEINTRESOURCE(IDR_BIN1), L"bin");
-	if (hResource)
-	{
-		HGLOBAL binGlob = LoadResource(hmodule, hResource);
-
-		if (binGlob)
-		{
-			void *binData = LockResource(binGlob);
-
-			if (binData)
-			{
-				HANDLE file;
-				strcat_s(path, sizeof(path), "\\Drivers\\");
-				strcat_s(path, sizeof(path), szDriverSys);
-
-				file = CreateFileA(path,
-					GENERIC_WRITE,
-					0,
-					NULL,
-					CREATE_ALWAYS,
-					0,
-					NULL);
-
-				if (file)
-				{
-					DWORD size, written;
-
-					size = SizeofResource(hmodule, hResource);
-					WriteFile(file, binData, size, &written, NULL);
-					CloseHandle(file);
-
-				}
-			}
-		}
-	}
-
-	Mgr = OpenSCManager(NULL, NULL, SC_MANAGER_ALL_ACCESS);
-	if (Mgr == NULL)
-	{							//No permission to create service
-		if (GetLastError() == ERROR_ACCESS_DENIED)
-		{
-			return 5;  // error access denied
-		}
-	}
-	else
-	{
-		char szFullPath[MAX_PATH] = "System32\\Drivers\\\0";
-		strcat_s(szFullPath, MAX_PATH, szDriverSys);
-		Ser = CreateServiceA(Mgr,
-			pszDriver,
-			pszDriver,
-			SERVICE_ALL_ACCESS,
-			SERVICE_KERNEL_DRIVER,
-			SERVICE_SYSTEM_START,
-			SERVICE_ERROR_NORMAL,
-			szFullPath,
-			NULL,
-			NULL,
-			NULL,
-			NULL,
-			NULL
-			);
-	}
-	CloseServiceHandle(Ser);
-	CloseServiceHandle(Mgr);
-
-	return 0;
-}
-
-HANDLE hdriver = NULL;
-/// !!! was LPCTSTR
-// start driver service
-int start(LPCSTR pszDriver)
-{
-	SC_HANDLE  Mgr;
-	SC_HANDLE  Ser;
-
-	Mgr = OpenSCManager(NULL, NULL, SC_MANAGER_ALL_ACCESS);
-
-	if (Mgr == NULL)
-	{							//No permission to create service
-		if (GetLastError() == ERROR_ACCESS_DENIED)
-		{
-			Mgr = OpenSCManager(NULL, NULL, GENERIC_READ);
-			Ser = OpenServiceA(Mgr, pszDriver, GENERIC_EXECUTE);
-			if (Ser)
-			{    // we have permission to start the service
-				if (!StartService(Ser, 0, NULL))
-				{
-					CloseServiceHandle(Ser);
-					return 4; // we could open the service but unable to start
-				}
-
-			}
-
-		}
-	}
-	else
-	{// Successfuly opened Service Manager with full access
-		//Ser = OpenServiceA(Mgr, pszDriver, GENERIC_EXECUTE);
-		Ser = OpenService(Mgr, L"hwinterfacex64", GENERIC_EXECUTE);
-		if (Ser)
-		{
-			//if (!StartServiceA(Ser, 0, NULL))
-			if (!StartService(Ser, 0, NULL))
-			{
-				CloseServiceHandle(Ser);
-				return 3; // opened the Service handle with full access permission, but unable to start
-			}
-			else
-			{
-				CloseServiceHandle(Ser);
-				return 0;
-			}
-		}
-	}
-	return 1;
-}
-
-#define DRIVERNAMEx64 "hwinterfacex64\0"
-#define DRIVERNAMEi386 "hwinterface\0"
-// open driver - after starting service?
-int Opendriver(BOOL bX64)
-{
-	OutputDebugStringW(L"Attempting to open InpOut driver...\n");
-
-	char szFileName[MAX_PATH] = { NULL };
-	if (bX64)
-		strcpy_s(szFileName, MAX_PATH, "\\\\.\\hwinterfacex64");	//We are 64bit...
-	else
-		strcpy_s(szFileName, MAX_PATH, "\\\\.\\hwinterface");		//We are 32bit...
-
-	hdriver = CreateFileA(szFileName,
-		GENERIC_READ | GENERIC_WRITE,
-		0,
-		NULL,
-		OPEN_EXISTING,
-		FILE_ATTRIBUTE_NORMAL,
-		NULL);
-
-	if (hdriver == INVALID_HANDLE_VALUE)
-	{
-		if (start(bX64 ? DRIVERNAMEx64 : DRIVERNAMEi386))
-		{
-			inst(bX64 ? DRIVERNAMEx64 : DRIVERNAMEi386);
-			start(bX64 ? DRIVERNAMEx64 : DRIVERNAMEi386);
-
-			hdriver = CreateFileA(szFileName,
-				GENERIC_READ | GENERIC_WRITE,
-				0,
-				NULL,
-				OPEN_EXISTING,
-				FILE_ATTRIBUTE_NORMAL,
-				NULL);
-
-			if (hdriver != INVALID_HANDLE_VALUE)
-			{
-				OutputDebugStringA("Successfully opened ");
-				OutputDebugStringA(bX64 ? DRIVERNAMEx64 : DRIVERNAMEi386);
-				OutputDebugStringA(" driver");
-				return 0;
-			}
-		}
-		return 1;
-	}
-	OutputDebugStringA("Successfully opened ");
-	OutputDebugStringA(bX64 ? DRIVERNAMEx64 : DRIVERNAMEi386);
-	OutputDebugStringA(" driver");
-	return 0;
-}
-
-#define IOCTL_READ_PORT_UCHAR	 -1673519100 //CTL_CODE(40000, 0x801, METHOD_BUFFERED, FILE_ANY_ACCESS)
-#define IOCTL_WRITE_PORT_UCHAR	 -1673519096 //CTL_CODE(40000, 0x802, METHOD_BUFFERED, FILE_ANY_ACCESS)
-void _stdcall Out32(short PortAddress, short data)
-{
-		unsigned int error;
-		DWORD BytesReturned;
-		BYTE Buffer[3];
-		unsigned short * pBuffer;
-		pBuffer = (unsigned short *)&Buffer[0];
-		*pBuffer = LOWORD(PortAddress);
-		Buffer[2] = LOBYTE(data);
-
-		if (!DeviceIoControl(hdriver,
-			IOCTL_WRITE_PORT_UCHAR,
-			&Buffer,
-			3,
-			NULL,
-			0,
-			&BytesReturned,
-			NULL)){
-
-			error = GetLastError();
-
-			LPTSTR lpMsgBuf;
-
-			FormatMessage(
-				FORMAT_MESSAGE_ALLOCATE_BUFFER |
-				FORMAT_MESSAGE_FROM_SYSTEM |
-				FORMAT_MESSAGE_IGNORE_INSERTS,
-				NULL,
-				error,
-				MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-				(LPTSTR)&lpMsgBuf,
-				0, NULL);
-
-			printf("Write error %ld :", error);
-			wprintf(L"%s", lpMsgBuf);
-		}
-}
 
 #define SRVICE_PERMISS (SC_MANAGER_CREATE_SERVICE | SERVICE_START | SERVICE_STOP | DELETE | SERVICE_CHANGE_CONFIG)
 int write_ltp_logic_analyzer(){
@@ -570,10 +337,11 @@ int get_image(){
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	//Opendriver(true);
-	//Out32(0x0378, 0xFFFF);
+	// InpOut32 - way
+	Opendriver(true);
+	Out32(0x0378, 0xFFFF);
 
-	write_ltp_logic_analyzer();
+	//write_ltp_logic_analyzer();
 
 	//write_ltp();
 	//get_image();
