762aee2b37cb916a8f2dc28f00dd219b831e81a1
Igor Gridchyn <igridchyn@gmail.com>
2014-06-23 12:57:05 +0200

    integer with increased precision KDE

--- sdl_example/KDClusteringProcessor.cpp
+++ sdl_example/KDClusteringProcessor.cpp
@@ -20,10 +20,19 @@ KDClusteringProcessor::KDClusteringProcessor(LFPBuffer *buf, const unsigned int 
 	obs_spikes_.resize(tetrn);
 	knn_cache_.resize(tetrn);
 	spike_place_fields_.resize(tetrn);
+	ann_points_int_.resize(tetrn);
+	spike_coords_int_.resize(tetrn);
 
 	for (int t = 0; t < tetrn; ++t) {
 		ann_points_[t] = annAllocPts(MIN_SPIKES, DIM);
 		spike_place_fields_[t].reserve(MIN_SPIKES);
+
+		ann_points_int_[t] = new int*[MIN_SPIKES];
+		for (int d = 0; d < MIN_SPIKES; ++d) {
+			ann_points_int_[t][d] = new int[DIM];
+		}
+
+		spike_coords_int_[t] = arma::Mat<int>(MIN_SPIKES, 2, arma::fill::zeros);
 	}
 }
 
@@ -36,31 +45,58 @@ void KDClusteringProcessor::build_pax_(const unsigned int tetr, const unsigned i
 
 	for (int xb = 0; xb < NBINS; ++xb) {
 		for (int yb = 0; yb < NBINS; ++yb) {
-			double kern_sum = .0f;
+			// order of >= 30
+			long long kern_sum = 0;
 
 			for (int ni = 0; ni < NN_K; ++ni) {
 				kern_sum += kern_(spikei, knn_cache_[tetr][spikei][ni], tetr);
 			}
 
-			pf(xb, yb) = kern_sum;
+			// scaled by MULT_INT ^ 2
+			pf(xb, yb) = (double)kern_sum / (MIN_SPIKES ^ 2);
 		}
 	}
 
 	spike_place_fields_[tetr][spikei] = pf;
 }
 
-double inline KDClusteringProcessor::kern_(const unsigned int spikei1, const unsigned int spikei2, const unsigned int tetr) {
-	// TODO: implement efficiently
-	double sum = .0f;
-	sum += (obs_spikes_[tetr][spikei1]->x - obs_spikes_[tetr][spikei2]->x) / X_STD;
-	sum += (obs_spikes_[tetr][spikei1]->y - obs_spikes_[tetr][spikei2]->y) / Y_STD;
+int inline KDClusteringProcessor::kern_(const unsigned int spikei1, const unsigned int spikei2, const unsigned int tetr) {
+	// TODO: implement efficiently (integer with high precision)
+	// TODO: check for the overlow (MULT order X DIM X MAX(coord order / feature sorder) X squared = (10 + 1 + 2) * 2 = 26 < 32 bit )
 
-	ANNcoord *pcoord1 = ann_points_[tetr][spikei1], *pcoord2 = ann_points_[tetr][spikei2];
+	int sum = 0;
+//	sum += (obs_spikes_[tetr][spikei1]->x - obs_spikes_[tetr][spikei2]->x) / X_STD;
+//	sum += (obs_spikes_[tetr][spikei1]->y - obs_spikes_[tetr][spikei2]->y) / Y_STD;
+
+	int *pcoord1 = ann_points_int_[tetr][spikei1], *pcoord2 = ann_points_int_[tetr][spikei2];
 	for (int d = 0; d < DIM; ++d, ++pcoord1, ++pcoord2) {
-		sum += (*pcoord1 - *pcoord2) * (*pcoord1 - *pcoord2);
+		sum += (*pcoord1 - *pcoord2) ^ 2;
 	}
+	sum *=X_STD;
 
-	return - sum / 2.0;
+	// !!! ASSUMING THAT X_STD == Y_STD don't divide but rather multiply in sum by X_STD
+	// X coordinate
+	sum += (spike_coords_int_[tetr](spikei1, 0) - spike_coords_int_[tetr](spikei2, 0)) ^ 2;
+	// Y coordinate
+	sum += (spike_coords_int_[tetr](spikei1, 1) - spike_coords_int_[tetr](spikei2, 1)) ^ 2;
+
+//	ANNcoord *pcoord1 = ann_points_[tetr][spikei1], *pcoord2 = ann_points_[tetr][spikei2];
+
+	// unroll loop - 20% faster
+//	sum += (*(pcoord1++) - *(pcoord2++)) ^ 2;
+//	sum += (*(pcoord1++) - *(pcoord2++)) ^ 2;
+//	sum += (*(pcoord1++) - *(pcoord2++)) ^ 2;
+//	sum += (*(pcoord1++) - *(pcoord2++)) ^ 2;
+//	sum += (*(pcoord1++) - *(pcoord2++)) ^ 2;
+//	sum += (*(pcoord1++) - *(pcoord2++)) ^ 2;
+//	sum += (*(pcoord1++) - *(pcoord2++)) ^ 2;
+//	sum += (*(pcoord1++) - *(pcoord2++)) ^ 2;
+//	sum += (*(pcoord1++) - *(pcoord2++)) ^ 2;
+//	sum += (*(pcoord1++) - *(pcoord2++)) ^ 2;
+//	sum += (*(pcoord1++) - *(pcoord2++)) ^ 2;
+//	sum += (*(pcoord1++) - *(pcoord2++)) ^ 2;
+
+	return - (sum >> 1);
 }
 
 void KDClusteringProcessor::process(){
@@ -83,14 +119,17 @@ void KDClusteringProcessor::process(){
 			}
 			delete dists;
 
-
-
 			// compute p(a_i, x) for all spikes (from neighbours
+			time_t start = clock();
 			for (int p = 0; p < total_spikes_[tetr]; ++p) {
 				// DEBUG
 				if (!(p % 500)){
-					std::cout << p << " place fields built ...\n";
-					if (p > 0)
+					std::cout.precision(2);
+					std::cout << p << " place fields built, last 500 in " << (clock() - start)/ (float)CLOCKS_PER_SEC << " sec....\n";
+					start = clock();
+
+				// for profiling
+					if (p > 2500)
 						exit(0);
 				}
 
@@ -104,6 +143,11 @@ void KDClusteringProcessor::process(){
 				// TODO: tetrode channels
 				for(int chan=0; chan < 4; ++chan){
 					ann_points_[tetr][total_spikes_[tetr]][chan * 3 + pc] = spike->pc[chan][pc];
+
+					// save integer with increased precision for integer KDE operations
+					ann_points_int_[tetr][total_spikes_[tetr]][chan * 3 + pc] = (int)round(spike->pc[chan][pc] * MULT_INT);
+					spike_coords_int_[tetr](total_spikes_[tetr], 0) = (int)round(spike->x * MULT_INT);
+					spike_coords_int_[tetr](total_spikes_[tetr], 1) = (int)round(spike->y * MULT_INT);
 				}
 			}
 
--- sdl_example/KDClusteringProcessor.h
+++ sdl_example/KDClusteringProcessor.h
@@ -24,16 +24,23 @@ class KDClusteringProcessor: public LFPProcessor {
 	const unsigned int DIM = 12;
 
 	// TODO: parametrize
-	const unsigned int NN_K = 1000;
+	const unsigned int NN_K = 100;
 	const unsigned int NN_EPS = 0.1;
 	const unsigned int NBINS = 20;
 	const unsigned int BIN_SIZE = 5;
 
-	const double X_STD = 100;
-	const double Y_STD = 100;
+	const int X_STD = 100;
+	const int Y_STD = 100;
+
+	// TODO: test for integer overflow in KDE operations
+	const unsigned int MULT_INT = 1024;
 
 	std::vector<ANNkd_tree*> kdtrees_;
 	std::vector<ANNpointArray> ann_points_;
+
+	// for integer computations with increased precision (multiplier = MULT_INT)
+	std::vector< int **  > ann_points_int_;
+	std::vector< arma::Mat<int> > spike_coords_int_;
 
 	// knn in the train set [tetrode][point]
 	std::vector<std::vector<ANNidx*> > knn_cache_;
@@ -44,7 +51,7 @@ class KDClusteringProcessor: public LFPProcessor {
 
 	// build p(a_i, x)
 	void build_pax_(const unsigned int tetr, const unsigned int spikei);
-	double inline kern_(const unsigned int spikei1, const unsigned int spikei2, const unsigned int tetr);
+	int inline kern_(const unsigned int spikei1, const unsigned int spikei2, const unsigned int tetr);
 
 public:
 	KDClusteringProcessor(LFPBuffer *buf, const unsigned int num_spikes);
