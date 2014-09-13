1c6ec6148cef632de83c8a9085e3e4e0f09899d8
Igor Gridchyn <igridchyn@gmail.com>
2014-09-13 22:20:01 +0200

    Polygon clsuters for online clustering in PCA display waveshape display following pca display () supposed always to be used together

--- sdl_example/SDLPCADisplayProcessor.cpp
+++ sdl_example/SDLPCADisplayProcessor.cpp
@@ -33,6 +33,10 @@ SDLPCADisplayProcessor::SDLPCADisplayProcessor(LFPBuffer *buffer, std::string wi
 , time_end_(buffer->SAMPLING_RATE * 60)
 {
     nchan_ = buffer->tetr_info_->channels_numbers[target_tetrode];
+
+    for(int t=0; t < buffer->tetr_info_->tetrodes_number; ++t){
+    	polygon_clusters_.push_back(std::vector<PolygonCluster>());
+    }
 }
 
 void SDLPCADisplayProcessor::process(){
@@ -67,6 +71,18 @@ void SDLPCADisplayProcessor::process(){
         x = spike->pc[comp1_ % nchan_][comp1_ / nchan_]/scale_ + shift_;
         y = spike->pc[comp2_ % nchan_][comp2_ / nchan_]/scale_ + shift_;
 
+        // polygon cluster
+        // TODO use scaled coordinates
+        if (spike->cluster_id_ == -1){
+        	for (int i=0; i < polygon_clusters_[target_tetrode_].size(); ++i){
+        		// TODO use other dimensions if cluster has the other one
+        		if (polygon_clusters_[target_tetrode_][i].Contains(x, y)){
+        			spike->cluster_id_ = i + 1;
+        		}
+        	}
+        }
+
+
         // time
         // TODO: check numeration
         if (comp1_ == 15){
@@ -98,25 +114,97 @@ void SDLPCADisplayProcessor::process(){
         SDL_RenderDrawLine(renderer_, 0, shift_, window_width_, shift_);
         SDL_RenderDrawLine(renderer_, shift_, 0, shift_, window_height_);
 
+        // polygon vertices
+        for(int i=0; i < polygon_x_.size(); ++i){
+        	SDL_SetRenderDrawColor(renderer_, 0, 255, 0, 255);
+        	int x = polygon_x_[i];
+        	int y = polygon_y_[i];
+        	int w = 3;
+        	SDL_RenderDrawLine(renderer_, x-w, y, x+w, y);
+        	SDL_RenderDrawLine(renderer_, x, y-w, x, y+w);
+
+        	if (i > 0){
+        		int px = polygon_x_[i-1];
+        		int py = polygon_y_[i-1];
+        		SDL_RenderDrawLine(renderer_, px, py, x, y);
+        	}
+        }
+        if (polygon_closed_){
+        	SDL_RenderDrawLine(renderer_, polygon_x_[0], polygon_y_[0], polygon_x_[polygon_x_.size() - 1], polygon_y_[polygon_y_.size() - 1]);
+        }
+
         SDL_RenderPresent(renderer_);
     }
 }
 
 void SDLPCADisplayProcessor::process_SDL_control_input(const SDL_Event& e){
+	if (e.type == SDL_MOUSEBUTTONDOWN){
+		if (e.button.button == SDL_BUTTON_LEFT){
+			std::cout << "Left button at " << e.button.x << ", " << e.button.y << "\n";
+			if (!polygon_closed_){
+				polygon_x_.push_back(e.button.x);
+				polygon_y_.push_back(e.button.y);
+			}else{
+				polygon_closed_ = false;
+				polygon_x_.clear();
+				polygon_y_.clear();
+			}
+		}else if(e.button.button == SDL_BUTTON_MIDDLE){
+			polygon_closed_ = true;
+			buffer->spike_buf_no_disp_pca = buffer->SPIKE_BUF_HEAD_LEN;
+
+
+		}
+	}
+
     if( e.type == SDL_KEYDOWN )
     {
         bool need_redraw = true;
         
         SDL_Keymod kmod = SDL_GetModState();
         
+        // select clusters from 10 to 29
         int shift = 0;
         if (kmod & KMOD_LSHIFT){
             shift = 10;
+        } else{
+        	if (kmod & KMOD_RSHIFT){
+        		shift = 20;
+        	}
         }
-        
+
         //Select surfaces based on key press
         switch( e.key.keysym.sym )
         {
+        	// polygon operations
+        	case SDLK_a:
+        		polygon_clusters_[target_tetrode_].push_back(PolygonCluster(PolygonClusterProjection(polygon_x_, polygon_y_, comp1_, comp2_)));
+        		buffer->Log("Created new polygon cluster, total clusters = ", polygon_clusters_[target_tetrode_].size());
+
+        		polygon_closed_ = false;
+        		polygon_x_.clear();
+        		polygon_y_.clear();
+        		break;
+
+        	// D: delete all clusters
+        	case SDLK_d:
+        		if (kmod & KMOD_LSHIFT){
+        			polygon_clusters_[target_tetrode_].clear();
+        			buffer->Log("Deleted all clusters");
+
+        			// TODO ? coordinate with model clustering
+        			for(int si = buffer->SPIKE_BUF_HEAD_LEN; si < buffer->spike_buf_no_disp_pca; ++si){
+        				Spike *spike = buffer->spike_buffer_[si];
+        				if (spike->tetrode_ == target_tetrode_){
+        					spike->cluster_id_ = -1;
+        				}
+        			}
+
+        		}else{
+        			need_redraw = false;
+        		}
+        		break;
+
             case SDLK_ESCAPE:
                 exit(0);
                 break;
@@ -212,4 +300,14 @@ void SDLPCADisplayProcessor::process_SDL_control_input(const SDL_Event& e){
             SDL_RenderPresent(renderer_);
         }
     }
+}
+
+void SDLPCADisplayProcessor::SetDisplayTetrode(const unsigned int& display_tetrode) {
+	target_tetrode_ = display_tetrode;
+	ReinitScreen();
+
+	buffer->spike_buf_no_disp_pca = buffer->SPIKE_BUF_HEAD_LEN;
+
+	polygon_x_.clear();
+	polygon_y_.clear();
 }
