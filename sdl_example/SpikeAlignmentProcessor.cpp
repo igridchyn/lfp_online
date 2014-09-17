715354d639f9f242b7a12b729daa64f6f9536867
Igor Gridchyn <igridchyn@gmail.com>
2014-09-17 16:33:45 +0200

    Read waveshape right after alignment and not after next spike detection

--- sdl_example/SpikeAlignmentProcessor.cpp
+++ sdl_example/SpikeAlignmentProcessor.cpp
@@ -78,39 +78,30 @@ void SpikeAlignmentProcessor::process(){
 		if (peak_pos - prev_spike_pos_[tetrode] > 16 || prev_spike_[tetrode] == NULL)
     	{
             if (prev_spike_[tetrode] != NULL){
-                // DEBUG
-                // printf("Aligned spike pos: %d\n", prev_spike_pos_);
-                // printf("%d ", prev_spike_pos_);
-                
-            	int buf_start_shift = buffer->buf_pos - (buffer->last_pkg_id - prev_spike_pos_[tetrode])
-                            		- Spike::WS_LENGTH_ALIGNED/ 2 - 1;
-
-                for(int ch=0; ch < num_of_ch; ++ch){
-                    memcpy(prev_spike_[tetrode]->waveshape[ch],
-                    		buffer->filtered_signal_buf[buffer->tetr_info_->tetrode_channels[prev_spike_[tetrode]->tetrode_][ch]]
-                    		+ buf_start_shift,
-                    		Spike::WS_LENGTH_ALIGNED * sizeof(int));
-                    
-                    // DEBUG
-                    //                    printf("Waveshape, %d, ch. %d: ", spike->pkg_id_, ch);
-                    //                    for (int i=0; i<spike->WS_LENGTH_ALIGNED;++i){
-                    //                        printf("%d ", spike->waveshape[ch][i]);
-                    //                    }
-                    //                    printf("\n");
-
-                    // if (buffer->buf_pos - (buffer->last_pkg_id - prev_spike_pos_[tetrode]) - Spike::WS_LENGTH_ALIGNED/ 2 - 1 < 0 ||
-                    int diff = buffer->buf_pos -
-                    		(buffer->last_pkg_id - prev_spike_pos_[tetrode]) +
-                    		Spike::WS_LENGTH_ALIGNED/2 - 1;
-                    if( diff > buffer->LFP_BUF_LEN + 100 || buf_start_shift < 0)//- buffer->BUF_HEAD_LEN)
-                    	std::cout << "WTF ??? " << buffer->spike_buf_nows_pos << "\n";
-                }
                 prev_spike_[tetrode]->aligned_ = true;
             }
             
             prev_spike_pos_[tetrode] = peak_pos;
             prev_max_val_[tetrode] = max_val;
             prev_spike_[tetrode] = spike;
+            // copy waveshape right away
+            if (prev_spike_[tetrode] != NULL){
+            	int buf_start_shift = buffer->buf_pos - (buffer->last_pkg_id - prev_spike_pos_[tetrode])
+                                        				- Spike::WS_LENGTH_ALIGNED/ 2 - 1;
+
+            	for(int ch=0; ch < num_of_ch; ++ch){
+            		memcpy(prev_spike_[tetrode]->waveshape[ch],
+            				buffer->filtered_signal_buf[buffer->tetr_info_->tetrode_channels[prev_spike_[tetrode]->tetrode_][ch]]
+            				                            + buf_start_shift,
+            				                            Spike::WS_LENGTH_ALIGNED * sizeof(int));
+
+            		// DEBUG
+            		int buf_start_shift = buffer->buf_pos - (buffer->last_pkg_id - prev_spike_pos_[tetrode])
+            		                            				- Spike::WS_LENGTH_ALIGNED/ 2 - 1;
+            		if(buf_start_shift < 0)
+            			std::cout << "WTF? Reading waveshape outside of filtered signal buffer...This is why I spend 8 hours of hateful debugging in Windows" << buffer->spike_buf_nows_pos << "\n";
+            	}
+            }
     	}
         // ??? otherwise - replace with current, if larger  ??? - still spiking
         else{
