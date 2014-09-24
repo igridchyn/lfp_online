c74c57ea7d8b19c59716940bfd426b04f45f5d51
Igor Gridchyn <igridchyn@gmail.com>
2014-09-24 18:10:06 +0200

    New config

--- sdl_example/main.cpp
+++ sdl_example/main.cpp
@@ -45,7 +45,7 @@ void draw_bin() {
 //	Config *config = new Config("../Res/spike_detection_jc117_0911_screen2.conf");
 //	Config *config = new Config("../Res/spike_detection_jc117_0914_screen3.conf");
 //	Config *config = new Config("../Res/nocon.conf");
-	Config *config = new Config("../Res/spike_detection_jc117_0916_screen0.conf");
+	Config *config = new Config("../Res/spike_detection_jc117_0921_6l.conf");
 
 //	Config *config = new Config("../Res/signal_display.conf");
 //	Config *config = new Config("../Res/spike_detection_jc11.conf");
