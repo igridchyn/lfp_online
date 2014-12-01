b4f9ef02e30bd32cdbc4e9bce6c907842b97ee7e
Igor Gridchyn <igridchyn@gmail.com>
2014-12-01 18:45:19 +0100

    Add decoding error for area outside of the start boxes

--- sdl_example/decoding_error.py
+++ sdl_example/decoding_error.py
@@ -8,8 +8,13 @@ f = open(argv[1])
 sum = 0
 ndist = 0
 
+sumnosb = 0
+nnosb = 0
+
 bsize = 7;
 errb = 0
+
+sbs = [[117, 6], [151, 139]]
 
 for line in f:
 	vals = line.split(' ')
@@ -23,6 +28,13 @@ for line in f:
 	if (vals[2] > 1000):
 		continue
 
+	distsb1 = sqrt((vals[2]-sbs[0][0])**2 + (vals[3]-sbs[0][1])**2)
+	distsb2 = sqrt((vals[2]-sbs[1][0])**2 + (vals[3]-sbs[1][1])**2)
+
+	if min(distsb1, distsb2) > 40:
+		sumnosb += dist
+		nnosb += 1
+
 	sum += dist
 	ndist += 1
 
@@ -33,4 +45,5 @@ for line in f:
 
 
 print "HMM error: ", sum/ndist
+print "HMM error outside of SB: ", sumnosb/nnosb
 print "Binning error: ", errb/ndist
