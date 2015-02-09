QA: fix SB locations; compute classification precision

sdl_example/decoding_error.py
@@ -13,12 +13,16 @@ ndist = 0
 sumnosb = 0
 nnosb = 0
 
-bsize = 7;
+bsize = 4
+nbins = 43
 errb = 0
 
-sbs = [[117, 6], [151, 139]]
+sbs = [[113, 10], [319, 137]]
 
 errs = []
+
+classn = 0.0
+classcorr = 0
 
 for line in f:
 	vals = line.split(' ')
@@ -35,7 +39,7 @@ for line in f:
 	distsb1 = sqrt((vals[2]-sbs[0][0])**2 + (vals[3]-sbs[0][1])**2)
 	distsb2 = sqrt((vals[2]-sbs[1][0])**2 + (vals[3]-sbs[1][1])**2)
 
-	if min(distsb1, distsb2) > 40:
+	if min(distsb1, distsb2) > 30:
 		sumnosb += dist
 		nnosb += 1
 
@@ -49,12 +53,18 @@ for line in f:
 
 	errb += sqrt((xb-xc)**2 + (yb-yc)**2)
 
+	# classification
+	classn += 1
+	if (vals[0] - nbins*bsize) * (vals[2] - nbins*bsize) > 0:
+		classcorr += 1
+
 n, bins, patches = P.hist(errs, 200, normed=0, histtype='stepfilled')
 # P.figure()
 
-print "HMM average error: ", sum/ndist
-print "HMM median error: ", np.median(np.array(errs))
-print "HMM error outside of SB: ", sumnosb/nnosb
+print "Average error: ", sum/ndist
+print "Median error: ", np.median(np.array(errs))
+print "Average error outside of SB: ", sumnosb/nnosb
+print ("Classification precision: %.1f%%") % (classcorr * 100 / classn)
 print "Binning error: ", errb/ndist
 
 P.show()