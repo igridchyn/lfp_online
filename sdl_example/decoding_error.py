31f059b9ff7d44355e61ebdef9fbaa1b73db32c4
Igor Gridchyn <igridchyn@gmail.com>
2015-04-15 15:55:25 +0200

    put evaluation into function

--- sdl_example/decoding_error.py
+++ sdl_example/decoding_error.py
@@ -4,75 +4,82 @@ import os, time
 import numpy as np
 import pylab as P
 
-print 'File Creation time: %s' % time.ctime(os.path.getctime(argv[1])) 
+def decoding_errors():
+	print 'File Creation time: %s' % time.ctime(os.path.getctime(argv[1])) 
 
-f = open(argv[1])
-sum = 0
-ndist = 0
+	f = open(argv[1])
+	sum = 0
+	ndist = 0
 
-sumnosb = 0
-nnosb = 0
+	sumnosb = 0
+	nnosb = 0
 
-bsize = 4
-nbins = 43
-errb = 0
+	bsize = 4
+	nbins = 43
+	errb = 0
 
-sbs = [[113, 10], [319, 137]]
+	sbs = [[113, 10], [319, 137]]
 
-errs = []
+	errs = []
 
-classn = 0.0
-classcorr = 0
+	classn = 0.0
+	classcorr = 0
 
-predmap = np.zeros((nbins, nbins*2))
-occmap = np.zeros((nbins, nbins*2))
+	predmap = np.zeros((nbins, nbins*2))
+	occmap = np.zeros((nbins, nbins*2))
 
-for line in f:
-	vals = line.split(' ')
-	vals = [float(v) for v in vals]
+	for line in f:
+		vals = line.split(' ')
+		vals = [float(v) for v in vals]
 
-	gtx = vals[2]
-	gty = vals[3]
-	px = vals[0]
-	py = vals[1]
+		gtx = vals[2]
+		gty = vals[3]
+		px = vals[0]
+		py = vals[1]
 
-	dist = sqrt( (px-gtx)**2 + (py-gty)**2 )
+		dist = sqrt( (px-gtx)**2 + (py-gty)**2 )
 
-	if gtx > 1000 or px < 5 or gtx < 0:
-		continue
+		if gtx > 1000 or px < 5 or gtx < 0:
+			continue
 
-	distsb1 = sqrt((gtx-sbs[0][0])**2 + (gty-sbs[0][1])**2)
-	distsb2 = sqrt((gtx-sbs[1][0])**2 + (gty-sbs[1][1])**2)
+		distsb1 = sqrt((gtx-sbs[0][0])**2 + (gty-sbs[0][1])**2)
+		distsb2 = sqrt((gtx-sbs[1][0])**2 + (gty-sbs[1][1])**2)
 
-	if min(distsb1, distsb2) > 30:
-		sumnosb += dist
-		nnosb += 1
+		if min(distsb1, distsb2) > 30:
+			sumnosb += dist
+			nnosb += 1
 
-	sum += dist
-	ndist += 1
+		sum += dist
+		ndist += 1
 
-	errs.append(dist)
+		errs.append(dist)
 
-	xb = (round(gtx / bsize) + 0.5) * bsize
-	yb = (round(gty / bsize) + 0.5) * bsize
+		xb = (round(gtx / bsize) + 0.5) * bsize
+		yb = (round(gty / bsize) + 0.5) * bsize
 
-	gtxb=round((gtx-bsize/2.0)/bsize)
-	gtyb=round((gty-bsize/2.0)/bsize)
-	# print gtyb, gtxb
-	occmap[min(gtyb, nbins-1), min(gtxb, 2*nbins-1)] += 1
+		gtxb=round((gtx-bsize/2.0)/bsize)
+		gtyb=round((gty-bsize/2.0)/bsize)
+		# print gtyb, gtxb
+		occmap[min(gtyb, nbins-1), min(gtxb, 2*nbins-1)] += 1
 
-	xpb = round((px-bsize/2)/bsize)
-	ypb = round((py-bsize/2)/bsize)
-	predmap[ypb, xpb] += 1
+		xpb = round((px-bsize/2)/bsize)
+		ypb = round((py-bsize/2)/bsize)
+		predmap[ypb, xpb] += 1
 
-	errb += sqrt((xb-gtx)**2 + (yb-gty)**2)
+		errb += sqrt((xb-gtx)**2 + (yb-gty)**2)
 
-	# classification
-	classn += 1
-	if (vals[0] - nbins*bsize) * (gtx - nbins*bsize) > 0:
-		classcorr += 1
+		# classification
+		classn += 1
+		if (vals[0] - nbins*bsize) * (gtx - nbins*bsize) > 0:
+			classcorr += 1
+
+	return (sum, ndist, errs, sumnosb, nnosb, classcorr, classn, errb)
+
+sum, ndist, errs, sumnosb, nnosb, classcorr, classn, errb = decoding_errors()
 
 # P.figure()
+
+print classcorr, classn
 
 print "Average error: ", sum/ndist
 print "Median error: ", np.median(np.array(errs))
