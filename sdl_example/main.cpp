0823e7c77827ef87f63ddd8fb6ce3f97af3d9a4c
Igor Gridchyn <igor.gridchin@gmail.com>
2015-12-11 10:44:37 +0100

    Win main update

--- sdl_example/main.cpp
+++ sdl_example/main.cpp
@@ -42,18 +42,21 @@ void usleep(__int64 usec)
 	// std::string cpath(R"(d:\Igor\soft\lfp_online\sdl_example\Res\signal_display_jc117_0919_4l_win.conf)");
 
 		// 1) dump spikes
-		// std::string cpath(R"(../Res/spike_dump_win.conf)");
+		//std::string cpath(R"(../Res/spike_dump_win.conf)");
 		// 2) diplay
-		//std::string cpath(R"(../Res/spike_display_win.conf)");
+		//// 3) build model
+		 //std::string cpath(R"(../Res/build_model_jc155_win.conf)");
+		// 4) detect assemplies online
+		//std::string cpath(R"(../Res/assembly_inhibition_jc155_win.conf)");
+		std::string cpath(R"(../Res/spike_display_win.conf)");
+		//std::string cpath(R"(../Res/synchrony_detection_win.conf)");
 
-		std::string cpath(R"(../Res/delay_test_win.conf)");
+		//std::string cpath(R"(../Res/delay_test_win.conf)");
 		//std::string cpath(R"(../Res/spike_detection_build_model_jc118_1003_8l_shift_WIN.conf)");
 		//std::string cpath(R"(../Res/decoding_online_jc118_1003_shift_WIN.conf)");
 		//std::string cpath(R"(../Res/trigger_jc140_win.conf)");
 		//std::string cpath(R"(../Res/spike_detection_build_model_jc140_WIN.conf)");
-		//std::string cpath(R"(../Res/decoding_online_jc140_win.conf)");
-
-		//system("d:/Igor/soft/lfp_online/sdl_example/lfponlinevs/Release/kde_win.exe");
+		//std::string cpath(R"(../Res/decoding_online_jc155_win.conf)");
 
 		Config *config = nullptr;
 
