591f34ed4fccd5840d26427e83f2487e023efc8a
Igor Gridchyn <igridchyn@gmail.com>
2016-02-17 14:55:40 +0100

    2 formats of SWR files: 1 entry = synchrony start / 3 entries = start + peak + end

--- sdl_example/SwReaderProcessor.cpp
+++ sdl_example/SwReaderProcessor.cpp
@@ -6,27 +6,53 @@
  */
 
 #include "SwReaderProcessor.h"
+#include "Utils.h"
 #include <fstream>
 
 SwReaderProcessor::SwReaderProcessor(LFPBuffer *buf)
 :SwReaderProcessor(buf,
-		buf->config_->getString("dat.path.base") + buf->config_->getString("swreader.ext")
+		buf->config_->getOutPath("swr.reader.path")
 		){}
 
 SwReaderProcessor::SwReaderProcessor(LFPBuffer *buf, std::string path)
 	: LFPProcessor(buf){
 
+	Utils::FS::CheckFileExistsWithError(path, buf);
 	std::ifstream swr_stream(path);
+
+	std::string swr_format_name_ = buffer->config_->getString("swr.reader.format");
+	if (swr_format_name_ == "start_peak_end"){
+		swr_format_ = SWR_FORMAT_START_PEAK_END;
+	} else if (swr_format_name_ == "synchrony_start"){
+		swr_format_ = SWR_FORMAT_SYNCHRONY_START;
+	} else {
+		Log("ERROR: Unrecognised SWR format, accepted values are : start_peak_end, synchrony_start");
+		exit(12432);
+	}
+
+	unsigned int pop_vec_len_pkg = buffer->POP_VEC_WIN_LEN * buffer->SAMPLING_RATE / 1000;
 
 	while(!swr_stream.eof()){
 		swrs_.push_back(std::vector<unsigned int>());
 
-		// read times of SWR beginning, peak and end
-		int b,p,e;
-		swr_stream >> b >> p >> e;
-		swrs_[swrs_.size() - 1].push_back(b);
-		swrs_[swrs_.size() - 1].push_back(p);
-		swrs_[swrs_.size() - 1].push_back(e);
+		switch(swr_format_){
+		case SWR_FORMAT_START_PEAK_END:
+			// read times of SWR beginning, peak and end
+			int b,p,e;
+			swr_stream >> b >> p >> e;
+			swrs_[swrs_.size() - 1].push_back(b);
+			swrs_[swrs_.size() - 1].push_back(p);
+			swrs_[swrs_.size() - 1].push_back(e);
+			break;
+
+		case SWR_FORMAT_SYNCHRONY_START:
+			unsigned int ss;
+			swr_stream >> ss;
+			swrs_[swrs_.size() - 1].push_back(ss > pop_vec_len_pkg ? ss - pop_vec_len_pkg : 0);
+			swrs_[swrs_.size() - 1].push_back(ss);
+			swrs_[swrs_.size() - 1].push_back(ss + pop_vec_len_pkg * 5);
+			break;
+		}
 	}
 
 	buffer->log_string_stream_ << "Read " << swrs_.size() << " SWRs.\n";
--- sdl_example/SwReaderProcessor.h
+++ sdl_example/SwReaderProcessor.h
@@ -10,11 +10,17 @@
 
 #include "LFPProcessor.h"
 
+enum SWR_Format{
+	SWR_FORMAT_START_PEAK_END,
+	SWR_FORMAT_SYNCHRONY_START
+};
+
 class SwReaderProcessor: public LFPProcessor {
 	std::vector<std::vector<unsigned int>> swrs_;
 
 	// current pkg_id is within or before this SW
 	unsigned int current_sw_ = 0;
+	SWR_Format swr_format_ = SWR_FORMAT_START_PEAK_END;
 
 public:
 	SwReaderProcessor(LFPBuffer *buf);
