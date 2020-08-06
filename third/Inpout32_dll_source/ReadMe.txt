c37daf4830119c4481992f3be291c7cb20acb9a8
Igor Gridchyn <igor.grichin@gmail.com>
2020-08-06 21:27:54 +0200

    remove 3rd party code

--- third/Inpout32_dll_source/ReadMe.txt
+++
@@ -1,3 +0,0 @@
-InpOut32Drv Driver Interface DLL
-
-Modified for x64 compatibility and build by Phillip Gibbons (Phil@Highrez.co.uk)
--- third/Inpout32_dll_source/inpout32.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-//Functions exported from DLL.
-//For easy inclusion is user projects.
-void	_stdcall Out32(short PortAddress, short data);
-short	_stdcall Inp32(short PortAddress);
